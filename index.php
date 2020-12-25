28e5eda7eb761c7431880528058be4e97de6992a
viettiencode <viettiencode@gmail.com>
2020-12-25 22:22:31 +0700

    Xin Chao

---
+++ index.php
@@ -0,0 +1,191 @@
+<!-- Tiến Developer -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tiến Developer</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="description" content="Một Sản Phẩm Chào Đón Xuân Năm Mới Của Tiến Ham Chơi" />
+	<meta property="og:title" content="Một Sản Phẩm Chào Đón Xuân Năm Mới Của Tiến Ham Chơi" />
+	<meta property="og:description" content="Một Sản Phẩm Chào Đón Xuân Năm Mới Của Tiến Ham Chơi" />
+    <meta name="keywords" content="Một Sản Phẩm Từ Tiến Developer / Tiến Ham Chơi"/>
+	<link href="https://fonts.googleapis.com/css?family=Roboto%3Aregular%2C100%7CRoboto+Slab%3A700%2Cregular&amp;subset=latin%2Call&amp;ver=4.9.8" rel="stylesheet">
+	<style>
+
+body{font-family:'Roboto Slab",sans-serif',sans-serif;color:#fff;background:url("thc_images/xuan2021.jpg");background-repeat:no-repeat;background-attachment:fixed;background-position:center;background-size:cover;-webkit-background-size:cover;-moz-background-size:cover;-o-background-size:cover;text-align:center;overflow: hidden;}	
+a { text-decoration: none; }
+#main-thc { position: relative; }
+#main-thc h1 span,#main-thc p span{font-weight:800}
+#main-thc p, #taitro {/*width:50%;*/margin:35px auto;font-size:18px;}
+#container-thc {padding: 70px 0; margin: 0}
+#clockdiv div{display:inline-block}
+
+#taitro {
+    color: #f7e158;
+    font-size: 1.5em;
+	text-shadow: 5px 2px 4px black;
+}
+
+.font-thc {
+    text-transform: uppercase;
+    font-weight: 300!important;
+}
+.font-thc strong {
+    font-weight: 700!important;
+}
+#timehientai { text-shadow: 5px 2px 4px #00BCD4; }
+loaievent{font-size:100%;font-weight:300!important;line-height:1.05;letter-spacing:.05em;text-transform:uppercase}
+timeend{text-shadow: 5px 2px 4px #FF5722;font-weight: bolder;font-family: "Dancing Script", sans-serif;/*font-size: 130%;*/margin: 0;font-size: 3.5em;display: block;}
+expectedtotakeplace {text-shadow: #FF9800 -1px -2px 0.5em;font-size: 11px;}
+.overlay{position:absolute;top:0;bottom:0;height:100%;left:-1px;right:-1px;background-color:rgba(0,0,0,0.15);transition:transform .3s,opacity .6s;pointer-events:none}
+.overlay{background-color:rgba(0,0,0,0.41)}
+
+/*
+p {font-family:'Roboto';font-style:normal;font-weight:400}
+h1 {font-family:'Roboto Slab';font-style:normal;font-weight:700}
+h2 {font-family:'Roboto Slab';font-style:normal;font-weight:400}
+h3 {font-family:'Roboto Slab';font-style:normal;font-weight:400}
+h4 {font-family:'Roboto Slab';font-style:normal;font-weight:400}
+h5 {font-family:'Roboto Slab';font-style:normal;font-weight:400}
+h6 {font-family:'Roboto Slab';font-style:normal;font-weight:400}
+*/
+
+endtime {font-family:'Roboto';font-style:normal;font-weight:400}
+loaievent {font-family:'Roboto'!important;font-style:normal!important;font-weight:100!important}
+
+/* UX Countdown Shortcode Styles */
+.ux-timer{border-spacing:5px;font-size:300%;}
+.ux-timer span{display:table-cell;font-weight:bolder;text-align:center;color:#FFF;line-height:80%;padding:.4em .1em .1em;margin:0 .05em .2em;border-radius:5px;background-color:rgba(0,0,0,.85)}
+.ux-timer span strong{font-size:40%;opacity:.7;text-transform:uppercase;display:block}
+.ux-timer.light span{text-shadow: 5px 2px 4px #607D8B;background-color:rgba(255,255,255,.85);color:#000}
+/* Button - Tiến Developer */
+#pur{text-shadow: 5px 2px 4px #ff2222;color:#fff;border:2px solid #fff;padding:10px;font-size:20px;-webkit-transition:0.5s ease-out;-moz-transition:0.5s ease-out;-ms-transition:0.5s ease-out;-o-transition:0.5s ease-out;transition:0.5s ease-out}
+#pur:hover{background:#fff;color:#111}
+/* Music - Tiến Developer */
+.music-box{position:fixed;width:300px;height:160px;z-index:120;bottom:30px;left:30px}
+.music-box a{color:#fff}
+@media (max-width: 991px) {
+.music-box{width:250px;height:120px;left:auto;left:30px}
+}
+.music-box .music-holder{width:100%;height:100%;border:2px solid #eee;position:absolute;left:0;top:-30px;visibility:hidden;opacity:0;-webkit-transition:all .5s ease-out;-o-transition:all .5s ease-out;transition:all .5s ease-out}
+.music-box .music-holder iframe{width:100%;height:100%}
+.music-box button{background:rgba(0,0,0,0.1);width:40px;height:40px;line-height:0;font-size:18px;font-size:1rem;color:#fff;border:1px solid #e6e6e6;outline:0;padding:0;margin:0;position:absolute;left:0;bottom:0;-webkit-border-radius:50%;-moz-border-radius:50%;-o-border-radius:50%;-ms-border-radius:50%;border-radius:50%}
+@media (max-width: 991px) {
+.music-box button{left:auto;left:0}
+}
+.music-box .toggle-music-box{top:-50px;visibility:visible;opacity:1}
+
+	</style>
+	
+</head>
+<body>
+ <div class="overlay"></div>
+ <div id="main-thc">
+  <div id="container-thc">
+   <loaievent>Sự Kiện Sắp Diễn Ra</loaievent>
+   <timeend>Thời Gian Diễn Ra Sự Kiện</timeend>
+   <expectedtotakeplace>...</expectedtotakeplace>
+    <div style="text-align: center; margin: 20px">
+	 <div id="clockdiv">
+	  <div class='ux-timer light'>
+	   <span style="width: 88px;"><thc id="hours">00</thc><strong>Giờ</strong></span>
+	   <span style="width: 62px;"><thc id="minutes">00</thc><strong>Phút</strong></span>
+	   <span style="width: 62px;"><thc id="seconds">00</thc><strong>Giây</strong></span>
+	  </div>
+	 </div>
+    </div>
+	<endtime class='font-thc'>Ngày Còn Lại</endtime>
+	<p id="timehientai">Thời Gian Hiện Tại...</p>
+	<a href="#" class="btn" id="pur">Tiến Developer - Tiến Ham Chơi</a>
+	<div id="taitro">Dự Án Thuộc: Tiến Ham Chơi (Tiến Developer)</div>
+  </div>
+ </div>
+	
+<div class='music-box'>
+<button class='music-box-toggle-btn'><img src="https://cdn.pixabay.com/photo/2019/08/11/18/27/icon-4399630_1280.png" width="30px"></button>
+<div class='music-holder'>
+<!--iframe allow='autoplay; encrypted-media' allowfullscreen='' frameborder='0' height='315' src='https://www.youtube.com/embed/WAg0jKqgmuI?controls=0&autoplay=1' width='560'></iframe-->
+<iframe allow='autoplay; encrypted-media' allowfullscreen='' frameborder='0' height='315' src='https://www.youtube.com/embed/GdzJCD3Rnbw?controls=0&autoplay=1' width='560'></iframe>
+</div>
+</div>
+
+<script  src="thc_js/jquery-1.12.3.min.js"></script>
+<script>
+WebFontConfig = {
+    google: {
+        families: ["Roboto+Slab:regular,700", "Roboto+Slab:regular,regular", "Roboto+Slab:regular,700", "Dancing+Script:regular,400", ]
+    }
+};
+</script>
+<script src='https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js'></script>
+<!-- Tiến Developer -->
+<script type="text/javascript">//<![CDATA[
+if($(".music-box").length) {var musicBtn = $(".music-box-toggle-btn"),musicBox = $(".music-holder");musicBtn.on("click", function() {musicBox.toggleClass("toggle-music-box");return false;})}
+
+$(document).ready(function(){
+	var t = [{ name: "Tân Sửu", m1: Date.parse("Feb 12 2021 00:00:00")}, { name: "Nhâm Dần", m1: Date.parse("Feb 12 2022 00:00:00")}],
+        e = t.length;
+	var today = new Date(), datevtien = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+	
+    setInterval(function(){
+		for(var n = (new Date).getTime(), a = 0; a < e; a++) {
+            var o = t[a].m1 - 864e5, r = t[a].m1 + 864e5, m = t[a].m1 + 2592e5;
+            if (n <= m) {
+                var i = t[a].m1, s = !1, c = a + 2021, g = t[a].name + " " + c,
+                    p = "Đếm ngược đến Tết " + g + "",
+					title = "Cùng đếm ngược tới Tết " + g + " - Tiến Developer (Tiến Ham Chơi)", l = "Sắp Bước Qua Năm Mới!!!",
+                    h = "Chúc", u = "Mừng", x = "Năm", T = "Mới",
+                    f = "Ngày", y = "Giờ", d = "Phút", C = "Giây";
+                if (n >= t[a].m1 && (i = m, n >= r ? l = "Vậy Là Sắp Hết Tết Rồi!!!" : (s = !0, l = "Tết Đến!!! Tết Rồi!!!", f = "Cung", y = "Chúc", d = "Tân", C = "Xuân")), !s) {
+                    var b = i - n;
+                    T = Math.floor(b / 1e3 % 60), //Giây
+					x = Math.floor(b / 1e3 / 60 % 60), //Phút
+					u = Math.floor(b / 36e5 % 24), //Giờ
+					h = Math.floor(b / 864e5) //Ngày
+                }
+				$("expectedtotakeplace").text("Cùng Đếm Ngược Chào Đón Một Năm Mới Nào!!!"),
+				$("loaievent").text(l), //Tên Sự Kiện
+				$("#timehientai").html("Hôm Nay " + datevtien + " Sắp Đến Tết Rồi!!!"), //Thời Gian Hiện Tại
+				$("endtime").html("<span style='text-shadow: #FFEB3B -1px -2px 0.5em;font-size: 170%;'>Và Chỉ Còn</span><strong><span id='days' style='text-shadow: #FF5722 -1px -2px 0.5em;font-size: 500%;'>0</span></strong><span style='text-shadow: #FFEB3B -1px -2px 0.5em;font-size: 170%;'> Ngày Nữa Là Đến Tết " + g + "!</span>"), //Ngày Còn Lại
+				$("#taitro").text("Một Dự Án Của: Tiến Ham Chơi Chào Đón Năm Mới " + g + ""), //Tài Trợ Bởi
+				$("#days").text(h), //Ngày Còn Lại
+				$("#hours").text(u), //Giờ Còn Lại
+				$("#minutes").text(x), //Phút Còn Lại
+				$("#seconds").text(T), //Giây Còn Lại
+				$("timeend").text(g), // Năm Con Giáp
+				n >= o && ($("html").css({
+                    background: "radial-gradient(ellipse at bottom, #efe81d 0%, #f80f1f 100%)"
+                })), document.title = title, $('meta[name="description"]').attr("content", "Còn bao nhiêu ngày nữa đến Tết Nguyên Đán " + g + "?"), $('meta[property="og\\:title"]').attr("content", p), $('meta[property="og\\:description"]').attr("content", "Còn bao nhiêu ngày nữa đến Tết Nguyên Đán " + g + "?");
+                break
+            }
+        }
+    }, 1e3)
+	
+});
+
+
+//Speed - Thời Gian Auto Thay Đổi Ảnh
+var num, temp=0, speed = 5000, preloads=[];
+//Thêm Ảnh Để Auto Thay Đổi
+preload(
+'thc_images/v0.jpg', 
+'thc_images/v1.jpg', 
+'thc_images/v2.jpg', 
+'thc_images/v3.jpg', 
+'thc_images/v4.jpg', 
+'thc_images/v5.jpg', 
+'thc_images/v6.jpg', 
+'thc_images/v7.jpg', 
+'thc_images/v8.jpg', 
+'thc_images/v9.jpg', 
+'thc_images/v10.jpg', 
+'thc_images/v11.jpg'
+);
+
+function preload(){for(var c=0;c<arguments.length;c++){preloads[preloads.length]=new Image();preloads[preloads.length-1].src=arguments[c]}}function rotateImages(){num=Math.floor(Math.random()*preloads.length);if(num==temp){rotateImages()}else{document.body.style.backgroundImage='url('+preloads[num].src+')';temp=num;setTimeout(function(){rotateImages()},speed)}}if(window.addEventListener){window.addEventListener('load',function(){setTimeout(function(){rotateImages()},speed)},false)}else{if(window.attachEvent){window.attachEvent('onload',function(){setTimeout(function(){rotateImages()},speed)})}}
+
+//]]></script>
+		
+</body>
+</html>
